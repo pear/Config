fd150f19f0088fc26b760454966b85e65b699c11
Bertrand Mansion <mansion@php.net>
2003-03-22 17:44:12 +0000

    Update example
    git-svn-id: http://svn.php.net/repository/pear/packages/Config/trunk@120908 c90b9560-bf6c-de11-be94-00142212c4b1

--- docs/Apache.php
+++ docs/Apache.php
@@ -1,57 +1,57 @@
 <?php
 /**
 * Config.php example with Apache container
-* @author 	Bertrand Mansion <bmansion@mamasam.com>
-* @package	Config
+* @author   Bertrand Mansion <bmansion@mamasam.com>
+* @package  Config
 */
 // $Id$
 
 require_once('Config.php');
 
 $datasrc = '/path/to/httpd.conf';
-$conf =& new Config('apache');
-$content =& $conf->parseConfig($datasrc);
+$conf = new Config();
+$content =& $conf->parseConfig($datasrc, 'apache');
 if (PEAR::isError($content)) {
-	die($content->getMessage());
+    die($content->getMessage());
 }
 
 // adding a new virtual-host
 
-$content->addItem('blank');
-$content->addItem('comment', '', 'My virtual host');
-$content->addItem('blank');
+$content->createBlank();
+$content->createComment('My virtual host');
+$content->createBlank();
 
-$vhost =& $content->addItem('section', 'VirtualHost', '127.0.0.1');
-$vhost->addItem('directive', 'DocumentRoot', '/usr/share/www');
-$vhost->addItem('directive', 'ServerName', 'www.mamasam.com');
-$location =& $vhost->addItem('section', 'Location', '/admin');
-$location->addItem('directive', 'AuthType', 'basic');
-$location->addItem('directive', 'Require', 'group admin');
+$vhost =& $content->createSection('VirtualHost', array('127.0.0.1:82'));
+$vhost->createDirective('DocumentRoot', '/usr/share/www');
+$vhost->createDirective('ServerName', 'www.mamasam.com');
+$location =& $vhost->createSection('Location', array('/admin'));
+$location->createDirective('AuthType', 'basic');
+$location->createDirective('Require', 'group admin');
 
 // adding some directives Listen
 
 if ($listen =& $content->getItem('directive', 'Listen')) {
-	$content->insertItem('directive', 'Listen', '82', 'after', $listen);
+    $res =& $content->createDirective('Listen', '82', null, 'after', $listen);
 } else {
-	$listen =& $content->insertItem('directive', 'Listen', '81', 'bottom');
-	if (PEAR::isError($listen)) {
-		die($listen->getMessage());
-	}
-	$content->insertItem('directive', 'Listen', '82', 'after', $listen);
+    $listen =& $content->createDirective('Listen', '81', null, 'bottom');
+    if (PEAR::isError($listen)) {
+        die($listen->getMessage());
+    }
+    $content->createDirective('Listen', '82', null, 'after', $listen);
 }
 
-echo '<pre>'.htmlspecialchars($content->toString()).'</pre>';
+echo '<pre>'.htmlspecialchars($content->toString('apache')).'</pre>';
 
 // Writing the files
 /*
-if (!PEAR::isError($write = $conf->writeConfig('/tmp/httpd.conf'))) {
-	echo 'done writing config<br>';
+if (!PEAR::isError($write = $conf->writeConfig('/tmp/httpd.conf', 'apache'))) {
+    echo 'done writing config<br>';
 } else {
-	die($write->getMessage());
+    die($write->getMessage());
 }
 
-if ($vhost->writeDatasrc('/tmp/vhost.conf')) {
-	echo 'done writing vhost<br>';
+if ($vhost->writeDatasrc('/tmp/vhost.conf', 'apache')) {
+    echo 'done writing vhost<br>';
 }
 */
 ?>
