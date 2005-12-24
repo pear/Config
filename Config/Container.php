5762560f404fdaaf3eb067fae81ef345383fa81e
Adam Ashley <aashley@php.net>
2005-12-24 02:28:42 +0000

    Fix for Bug #3137
    git-svn-id: http://svn.php.net/repository/pear/packages/Config/trunk@203593 c90b9560-bf6c-de11-be94-00142212c4b1

--- Config/Container.php
+++ Config/Container.php
@@ -401,7 +401,7 @@ class Config_Container {
         $count = 0;
         if (isset($name) && isset($type)) {
             for ($i = 0, $children = count($this->children); $i < $children; $i++) {
-                if ($this->children[$i]->name == $name && 
+                if ($this->children[$i]->name === $name && 
                     $this->children[$i]->type == $type) {
                     $count++;
                 }
@@ -419,7 +419,7 @@ class Config_Container {
         if (isset($name)) {
             // Some directives can have the same name
             for ($i = 0, $children = count($this->children); $i < $children; $i++) {
-                if ($this->children[$i]->name == $name) {
+                if ($this->children[$i]->name === $name) {
                     $count++;
                 }
             }
