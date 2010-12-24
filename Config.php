dc335a627844201382f049b3234b24495e699d15
Adam Harvey <aharvey@php.net>
2010-12-24 05:11:09 +0000

    Fix bug #13116 (Syntax Strict notice for references on constructors). Patch by Francisco Naranjo.
    git-svn-id: http://svn.php.net/repository/pear/packages/Config/trunk@306597 c90b9560-bf6c-de11-be94-00142212c4b1

--- Config.php
+++ Config.php
@@ -77,7 +77,7 @@ class Config {
     */
     function Config()
     {
-        $this->container =& new Config_Container('section', 'root');
+        $this->container = new Config_Container('section', 'root');
     } // end constructor
 
     /**
@@ -163,7 +163,7 @@ class Config {
             if ($rootContainer->getName() === 'root' && $rootContainer->getType() === 'section') {
                 $this->container =& $rootContainer;
             } else {
-                $this->container =& new Config_Container('section', 'root');
+                $this->container = new Config_Container('section', 'root');
                 $this->container->addItem($rootContainer);
             }
             return true;
