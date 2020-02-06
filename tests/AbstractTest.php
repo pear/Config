ebe47a9023721c21b35d4f290f5bf247ca135002
Jean Claveau <jean.claveau@gmail.com>
2020-02-06 16:52:48 +0100

    Support older PHP versions

--- tests/AbstractTest.php
+++ tests/AbstractTest.php
@@ -1,18 +1,4 @@
 <?php
-/**
- *
- */
-
 class AbstractTest extends \PHPUnit\Framework\TestCase
 {
-    public static function setUpBeforeClass() : void
-    {
-    }
-
-    public function setUp() : void
-    {
-        // echo( get_called_class() . '::' . $this->getName() ."\n" );
-    }
-
-    /**/
 }
